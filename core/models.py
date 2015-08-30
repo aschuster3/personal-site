ecee3094f92829fbaa7cc97a31b806ec88f64bab
Andrew <aschuster93@gmail.com>
2015-08-29 21:04:44 -0400

    Tweaked message model

--- core/models.py
+++ core/models.py
@@ -12,3 +12,6 @@ class Message(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.TextField()
+
+    def __str__(self):
+        return self.name
